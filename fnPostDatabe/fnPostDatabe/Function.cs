e416c73a4944141cf4851d8dd307c802bf171ad9
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-02-03 12:24:20 -0300

    creating azure functions to save to CosmosDB-Azure

---
+++ fnPostDatabe/fnPostDatabe/Function.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+
+namespace fnPostDatabe
+{
+    public class Function
+    {
+        private readonly ILogger<Function> _logger;
+
+        public Function(ILogger<Function> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("movie")]
+        [CosmosDBOutput("%DatabaseName%", "movies", Connection = "CosmosDBConnection", CreateIfNotExists = true, PartitionKey = "id")]
+        public async Task<object?> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            MovieRequest movie = null;
+
+            var content = await new StreamReader(req.Body).ReadToEndAsync();
+
+            try
+            {
+                movie = JsonConvert.DeserializeObject<MovieRequest>(content);
+            }
+            
+            catch(Exception ex)
+            {
+                return new BadRequestObjectResult("Error deserialize object: " + ex.Message);
+            }
+
+            return JsonConvert.SerializeObject(movie);
+        }
+    }
+}
